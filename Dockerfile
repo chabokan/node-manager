da7739d368f1d1fc7215ed04179dffd1ecf758aa
mohammad <mohammad_m69@yahoo.com>
2024-02-24 13:29:30 +0330

    update

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM docker.chabokan.net/python:3.9-buster
+
+ENV TZ=Asia/Tehran
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+WORKDIR /app
+
+RUN apt-get update && apt-get -y install cron nginx pigz duc default-mysql-client supervisor unar nano vim htop net-tools zip unzip iputils-ping \
+&& mkdir /backups && mkdir /builds
+
+RUN wget https://s3.ir-thr-at1.arvanstorage.ir/public-chabok/docker-latest.tgz && tar -xvzf docker-latest.tgz && mv docker/* /usr/bin/
+
+COPY requirements.txt /app/
+RUN pip install --no-cache-dir -r /app/requirements.txt
+
+ADD start.sh /
+RUN chmod +x /start.sh
+EXPOSE 80
+
+CMD ["/start.sh"]
---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -d /app/supervisor_conf ]; then
+  echo "> Copy Supervisor Configuration Directory ..."
+  cp -rf /app/supervisor_conf/* /etc/supervisor/conf.d/
+  echo "> Starting Supervisor ..."
+  for i in {1..5}; do {
+    service supervisor start
+    supervisorctl reread
+    supervisorctl update
+    supervisorctl start all
+  } && break || sleep 5; done
+else
+  if [ -f /app/supervisor.conf ]; then
+    echo "> Copy Supervisor Configuration ..."
+    cp -rf /app/supervisor.conf /etc/supervisor/conf.d/supervisor.conf
+    echo "> Starting Supervisor ..."
+    for i in {1..5}; do {
+      service supervisor start
+      supervisorctl reread
+      supervisorctl update
+      supervisorctl start all
+    } && break || sleep 5; done
+  fi
+fi
+
+echo "Run uvicorn Server"
+uvicorn "main:app" --host 0.0.0.0 --port 80 --workers 1 --log-level info
