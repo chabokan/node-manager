e61bb9f42fb62f20a4424ad5d3fdf66f62b78f46
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2024-03-30 12:34:12 +0330

    update

--- core/cron.py
+++ core/cron.py
@@ -12,22 +12,23 @@ from main import app
 @app.on_event("startup")
 @repeat_every(seconds=300)
 def server_sync() -> None:
-    server_info = get_system_info()
-    ip = get_server_ip()
     db = get_db()
-    data = {
-        "token": crud.get_setting(db, "token"),
-        "ram": server_info['ram']['count'],
-        "cpu": server_info['cpu']['total'],
-        "disk": server_info['all_disk_space'],
-        "ip": ip,
-        "ram_usage": server_info['ram']['used'],
-        "cpu_usage": server_info['cpu']['usage'],
-        "disk_usage": server_info['all_disk_usage'],
-        "disk_data": server_info['disk']
-    }
-    headers = {
-        "Content-Type": "application/json",
-    }
-    r = requests.post("https://hub.chabokan.net/fa/api/v1/servers/connect-server/", headers=headers,
-                      data=json.dumps(data))
+    if crud.get_setting(db, "token"):
+        server_info = get_system_info()
+        ip = get_server_ip()
+        data = {
+            "token": crud.get_setting(db, "token"),
+            "ram": server_info['ram']['count'],
+            "cpu": server_info['cpu']['total'],
+            "disk": server_info['all_disk_space'],
+            "ip": ip,
+            "ram_usage": server_info['ram']['used'],
+            "cpu_usage": server_info['cpu']['usage'],
+            "disk_usage": server_info['all_disk_usage'],
+            "disk_data": server_info['disk']
+        }
+        headers = {
+            "Content-Type": "application/json",
+        }
+        r = requests.post("https://hub.chabokan.net/fa/api/v1/servers/connect-server/", headers=headers,
+                          data=json.dumps(data))
