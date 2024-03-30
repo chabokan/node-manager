b940b7bdbb1f4cb0fa064275b037a8ff6953d6c1
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2024-03-30 14:12:43 +0330

    update

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from api.main import api_router
 from core.config import settings
 import models
 from core.db import engine
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://e0d86ba4f740f7b3bd16d5aa226f2545@sentry.chabokan.net/15",
+    traces_sample_rate=1.0,
+)
 
 app = FastAPI()
 app.include_router(api_router, prefix=settings.API_V1_STR)
