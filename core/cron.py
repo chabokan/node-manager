ea455e28da14258049a6a99200c92f78df72410e
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2024-03-30 14:43:14 +0330

    update

--- core/cron.py
+++ core/cron.py
@@ -13,25 +13,25 @@ from main import app
 @repeat_every(seconds=5, raise_exceptions=True, max_repetitions=1)
 def server_sync() -> None:
     db = next(get_db())
-    if crud.get_setting(db, "token"):
-        server_info = get_system_info()
-        ip = get_server_ip()
-        data = {
-            "token": crud.get_setting(db, "token").value,
-            "ram": server_info['ram']['total'],
-            "cpu": server_info['cpu']['count'],
-            "disk": server_info['all_disk_space'],
-            "ip": ip,
-            "ram_usage": server_info['ram']['used'],
-            "cpu_usage": server_info['cpu']['usage'],
-            "disk_usage": server_info['all_disk_usage'],
-            "disk_data": server_info['disk']
-        }
-        headers = {
-            "Content-Type": "application/json",
-        }
-
-        r = requests.post("https://hub.chabokan.net/fa/api/v1/servers/connect-server/", headers=headers,
-                          data=json.dumps(data))
-
-        Exception(r)
+    # if crud.get_setting(db, "token"):
+    #     server_info = get_system_info()
+    #     ip = get_server_ip()
+    #     data = {
+    #         "token": crud.get_setting(db, "token").value,
+    #         "ram": server_info['ram']['total'],
+    #         "cpu": server_info['cpu']['count'],
+    #         "disk": server_info['all_disk_space'],
+    #         "ip": ip,
+    #         "ram_usage": server_info['ram']['used'],
+    #         "cpu_usage": server_info['cpu']['usage'],
+    #         "disk_usage": server_info['all_disk_usage'],
+    #         "disk_data": server_info['disk']
+    #     }
+    #     headers = {
+    #         "Content-Type": "application/json",
+    #     }
+    #
+    #     r = requests.post("https://hub.chabokan.net/fa/api/v1/servers/connect-server/", headers=headers,
+    #                       data=json.dumps(data))
+    #
+    #     Exception(r)
