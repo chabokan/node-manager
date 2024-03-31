6546958792716f80851285933f90ba83689536f3
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2024-03-31 12:37:52 +0330

    update

--- core/cron.py
+++ core/cron.py
@@ -57,4 +57,4 @@ def get_jobs_from_hub() -> None:
     headers = {
         "Content-Type": "application/json",
     }
-    r = requests.post("http://0.0.0.0/api/v1/jobs/", headers=headers, timeout=15)
+    r = requests.get("http://0.0.0.0/api/v1/jobs/", headers=headers, timeout=15)
