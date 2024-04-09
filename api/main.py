298fa92702b54a36a17cb637c1dd9e1e1370489f
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2024-04-09 15:47:21 +0330

    update

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from api.routes import core, server
+from api.routes import core, server, services
 
 api_router = APIRouter()
 api_router.include_router(core.router, tags=["core"])
 api_router.include_router(server.router, prefix="/server", tags=["server"])
-api_router.include_router(server.router, prefix="/service", tags=["service"])
+api_router.include_router(services.router, prefix="/service", tags=["service"])
