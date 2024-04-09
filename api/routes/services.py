656bfa40903998d8b3adaad1ab77b534ba774f88
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2024-04-09 15:29:09 +0330

    update

--- api/routes/services.py
+++ api/routes/services.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends
 import crud
+from api.helper import service_logs
 from core.db import get_db
 
 router = APIRouter()
 
 
-@router.get("/logs/")
-async def logs(db=Depends(get_db)):
-
-    return
+@router.get("/{name}/logs/")
+async def logs(name, db=Depends(get_db)):
+    container_logs = service_logs(name)
+    return {"success": True, "logs": container_logs}
