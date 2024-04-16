d80bfbfee978dc181246183860f864a361aa7144
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2024-04-16 14:48:40 +0330

    update

--- update_core.sh
+++ update_core.sh
@@ -6,10 +6,4 @@ docker compose build web
 docker compose up -d
 docker compose restart web
 
-service supervisor stop
-sleep 3
-service supervisor start
-sleep 5
-supervisorctl reread
-supervisorctl update
-supervisorctl start all
+service cron restart
